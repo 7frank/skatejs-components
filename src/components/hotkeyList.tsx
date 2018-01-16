ce80364ec841b5e46355e6b626e1fc2d5c53fca8
Frank <frank.next@gmx.de>
2018-01-16 19:27:10 +0100

    hotkeylist with dependiencies to local hotkey package

--- src/components/hotkeyList.tsx
+++ src/components/hotkeyList.tsx
@@ -1,24 +1,94 @@
 import 'skatejs-web-components';
-import { Component, h, prop, props } from 'skatejs';
+import {Component, h, prop, props} from 'skatejs';
 
-export interface ButtonProps {
-  label: string;
+import * as styles from './hotkeyList.css'
+
+import {getRegistered,Hotkeys,showHotkeyList} from  "@nk/keyboard-interactions"
+
+export interface HotkeyListProps {
+    label: string;
+    deletion: boolean;
 }
 
-export class Button extends Component<ButtonProps> {
-  static get is() { return 's-button' }
-
-  static get props() {
-    return {
-      label: prop.string({ attribute: true }),
+export class HotkeyList extends Component<HotkeyListProps> {
+    static get is() {
+        return 'nk-hotkey-list'
     }
-  }
 
-  label: string;
+    static get props() {
+        return {
+            label: prop.string({attribute: true}),
+            deletion: prop.boolean({attribute: true}),
+        }
+    }
 
-  renderCallback() {
-    return <button>{this.label}</button>
-  }
+    label: string;
+    deletion: boolean;
+
+
+    onTagClick(arg) {
+
+
+      /*  Hotkeys('show help', 'h', function () {
+            showHotkeyList();
+        });*/
+
+        console.log("registered",this,getRegistered())
+
+        Hotkeys(arg.action, arg.combo,function(e){
+            e.stopPropagation()
+            e.preventDefault()
+
+            arg.handler(e)
+
+        })
+
+
+
+
 }
 
-customElements.define(Button.is, Button);
+
+    renderCallback() {
+
+
+        const tags = [
+            {
+                combo: "ctrl+s", action: "save File", handler: function () {
+                console.log("hello hotkeylist", arguments)
+            }, description: "a sample entry for the hotkeylist"
+            },
+            {
+                combo: "ctrl+alt+#", action: "custom action", handler: function () {
+                console.log("hello hotkeylist2", arguments)
+            }, description: "another sample entry"
+            },
+            {
+                combo: "ctrl+a", action: "select all", handler: function () {
+                console.log("hello hotkeylist3", arguments)
+            }, description: "more sample stuff"
+            },
+        ];
+        const allowDeletion = this.deletion ? 'deletion' : '';
+        const tagElements = tags.map(t => {
+            const tagContent = allowDeletion ? <span class='deletion'>{t.action}</span> :  <span>{t.action}</span>;
+
+            return <div  class={styles.row}>
+                {tagContent}
+                <input value={t.combo}></input>
+                <span class="description">{t.description}</span>
+                <button  onclick={()=>this.onTagClick(t)}>bind combo</button>
+            </div>;
+        });
+
+
+        /*    var rows = [];
+            for (var i = 0; i < 5; i++) {
+                rows.push(new Button());
+            }*/
+
+        return <div class={styles.list} >{tagElements}</div>
+    }
+}
+
+customElements.define(HotkeyList.is, HotkeyList);
